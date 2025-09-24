5c5871d30eef6b427caadb1d1428f9ea306cc21e
macnoblebk <macnoblebk@gmail.com>
2025-09-24 14:07:17 -0700

    Comments existing fields and methods

--- KMeansMPI.h
+++ KMeansMPI.h
@@ -16,6 +16,15 @@ public:
     static const int MAX_FIT_STEPS = 300;  // Maximum number of iterations before giving up
     static const int ROOT_PROCESS = 0;     // Rank of the root/coordinator process
 
+    /**
+     * @brief Equality operator for clusters
+     *
+     * Compares only centroids, regardless of the elements in the cluster.
+     *
+     * @param left Left operand
+     * @param right Right operand
+     * @return true if centroids are identical, false otherwise
+     */
     struct Cluster {
         Element centroid;                // Center (mean) of the elements in the cluster
         std::vector<int> elements;       // Indices of elements belonging to this cluster
@@ -27,22 +36,52 @@ public:
 
     using Clusters = std::array<Cluster, k>;
 
+  /**
+   * @brief Constructor
+   */
     KMeansMPI() : isVerboseMode(false) {}
 
+/**
+ * @brief Enable or disable verbose debug output
+ * @param verbose True to enable verbose output, false to disable
+ */
     void setVerboseMode(bool verbose) {
         isVerboseMode = verbose;
     }
 
+  /**
+   * @brief Expose the clusters to the client readonly
+   *
+   * Provides access to the final clustering results after fit() has been called.
+   *
+   * @return Reference to the array of clusters from the latest call to fit()
+   */
     virtual const Clusters& getClusters() {
         return clusters;
     }
 
+    /**
+     * @brief Main k-means clustering algorithm entry point
+     *
+     * This method is called by the ROOT process only. It initializes the data
+     * and calls fitWork() to perform the clustering in parallel.
+     *
+     * @param data Pointer to the array of data elements for k-means
+     * @param nData The number of data elements in the array
+     */
     virtual void fit(const Element* data, int nData) {
         elements = data;
         totalElements = nData;
         fitWork(ROOT_PROCESS);
     }
 
+    /**
+     * @brief Per-process work for fitting the k-means model
+     *
+     * This is the main worker method for all processes.
+     *
+     * @param rank Process rank within MPI_COMM_WORLD
+     */
     virtual void fitWork(int rank) {
         initializeClusteringProcess(rank);
         runClusteringIterations(rank);
