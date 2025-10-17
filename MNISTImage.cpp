a632a4c99e1ee3d5cb87f25380822b43be2dcd0d
macnoblebk <macnoblebk@gmail.com>
2025-10-17 14:55:26 -0700

    Adds functionality to calculate distance between this image and another

--- MNISTImage.cpp
+++ MNISTImage.cpp
@@ -12,3 +12,13 @@ char buffer[7];
 snprintf(buffer, sizeof(buffer), "%.6x", pixel << 16 | pixel << 8 | pixel);
 return {buffer};
 }
+
+
+double MNISTImage::euclideanDistance(const MNISTImage &other) const {
+    double sum = 0.0;
+    for (int i = 0; i < PIXELS_N; i++) {
+        double difference = static_cast<double>(pixels[i]) - static_cast<double>(other.pixels[i]);
+        sum += difference * difference;
+    }
+    return std::sqrt(sum);
+}
