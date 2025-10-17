2a43e1066430ae5836b36aab3e20b3520aa7c9a8
macnoblebk <macnoblebk@gmail.com>
2025-10-17 14:51:25 -0700

    Adds functionality to convert prixel value to hexadecimal color string

--- MNISTImage.cpp
+++ MNISTImage.cpp
@@ -4,3 +4,11 @@
 #include "MNISTImage.h"
 
 MNISTImage::MNISTImage(const Pixels pixels) noexcept: pixels(pixels) {}
+
+
+std::string MNISTImage::pixelToHex(int row, int col) const noexcept {
+u_char pixel = pixels[ROWS_N * row + col];
+char buffer[7];
+snprintf(buffer, sizeof(buffer), "%.6x", pixel << 16 | pixel << 8 | pixel);
+return {buffer};
+}
